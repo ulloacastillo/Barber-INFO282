c078287954fbb4a827f4b7b82fd4b6cd19dd2f30
ulloacastillo <jorgeulloacastillo@gmail.com>
2021-11-19 01:15:57 -0300

    empezando hacer home

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
+import Service from "./components/Service";
+import services from "./services.json";
+import React, { useState, Fragment } from "react";
 
-import Service from './components/Service';
-import services from './services.json'
-import React, {useState, Fragment} from "react";
+import Home from "./Home";
 
 function App() {
-  
   return (
     <Fragment>
-
+      <Home />
       <div className="row">
-        {services.map(service => (
-          <Service service={service} key={service.id}/>
+        {services.map((service) => (
+          <Service service={service} key={service.id} />
         ))}
-        
       </div>
-      
     </Fragment>
   );
 }
--- src/components/Service.js
+++ src/components/Service.js
@@ -1,36 +1,35 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-import BookingModal from './BookingModal';
+import BookingModal from "./BookingModal";
 
-import { Button } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
 
-const Service = ({service}) => {
+const Service = ({ service }) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    return(
-        <div className="col-md-3 m-3">
-            <div class="card">
-                <div class="card-header">
-                    {service.nombre}
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title">{service.nombre}</h5>
-                    <p class="card-text">{service.descripcion}</p>
-                        <Button variant="primary" onClick={handleShow}>
-                            Reserva +
-                        </Button> 
-                        <div className="row">
-                            <BookingModal show={show} service = {service} handleClose={handleClose}/>
-                        </div>
-                </div>
-            </div>
+  return (
+    <div className="col-md-3 m-3">
+      <div class="card h-100">
+        <div class="card-header">{service.nombre}</div>
+        <div class="card-body">
+          <h5 class="card-title">{service.nombre}</h5>
+          <p class="card-text">{service.descripcion}</p>
+          <Button variant="primary" onClick={handleShow}>
+            Reserva +
+          </Button>
+          <div className="row">
+            <BookingModal
+              show={show}
+              service={service}
+              handleClose={handleClose}
+            />
+          </div>
         </div>
-    
-        
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Service;
